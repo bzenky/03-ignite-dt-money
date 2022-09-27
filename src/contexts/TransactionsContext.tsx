72ab23cbc9ebc72743c3c255b7988a51cf5e93f0
Zenky <bzenky@gmail.com>
2022-09-27 20:47:57 -0300

    feat: context

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from "react"
+
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  category: string
+  price: number
+  createdAt: string
+}
+
+interface TransactionContextType {
+  transactions: Transaction[]
+}
+
+interface TransactionProviderProps {
+  children: React.ReactNode
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType)
+
+export function TransactionsProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
